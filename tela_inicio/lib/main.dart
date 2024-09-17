954840235eed3bed59af391b2d052494732f6bc5
Virgínia <cc22153@g.unicamp.br>
2024-09-17 16:53:18 -0300

    tela de menu inicial pronta

--- tela_inicio/lib/main.dart
+++ tela_inicio/lib/main.dart
@@ -17,50 +17,133 @@ class TelaInicio extends StatefulWidget {
 class TelaInicioState extends State<TelaInicio> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+    // tela lateral que exibe as opções
       drawer: Drawer(
-        child: Column(
+
+        backgroundColor: const Color.fromARGB(200, 158, 158, 158),
+        
+        child: Column( // coluna de componetes
+
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ListTile(
-              leading: Icon(Icons.attach_money, color: Colors.red),
-              title: Text('Menu Lateral 1'),
-              subtitle: Text('Selecione a opção no menu'),
-              onTap: () => print('Menu Lateral 1 clicado'),
+
+          children: [ // "filho" conteúdo de dentro
+
+              SizedBox(height: 20), // Adiciona um espaçamento
+        
+
+            ListTile(  // cria uma lista de titulos
+
+              leading: Icon(Icons.home, color: Colors.white),
+              title: Text('ínicio'),
+              textColor: Colors.white,
             ),
+
+
+
             ListTile(
-              leading: Icon(Icons.attach_money, color: Colors.red),
-              title: Text('Menu Lateral 2'),
-              subtitle: Text('Selecione a opção no menu'),
-              onTap: () => print('Menu Lateral 2 clicado'),
+
+              leading: Icon(Icons.book, color: Colors.white),
+              title: Text('Histórico'),
+               textColor: Colors.white,
+
             ),
-            SizedBox(height: 20), // Adiciona um espaçamento
-            ListTile(
-              title: Text('Opção 1'),
-              onTap: () => print('Opção 1 clicada'),
+
+             ListTile(
+
+              leading: Icon(Icons.restaurant, color: Colors.white),
+              title: Text('Produtos'),
+                textColor: Colors.white,
+           
             ),
-            ListTile(
-              title: Text('Opção 2'),
-              onTap: () => print('Opção 2 clicada'),
+
+             ListTile(
+
+              leading: Icon(Icons.restaurant_menu, color: Colors.white),
+              title: Text('Cardápio'),
+                textColor: Colors.white,
+            
             ),
+
+             ListTile(
+
+              leading: Icon(Icons.exit_to_app, color: Colors.white),
+              title: Text('Sair'),
+              textColor: Colors.white,        
+
+            ),
+
           ],
         ),
       ),
+      //titulo da tela
       appBar: AppBar(
+
         backgroundColor: Colors.grey,
+
         title: const Text("Início"),
+        
       ),
+
       body: SingleChildScrollView(
+
         padding: const EdgeInsets.all(50),
-        child: ElevatedButton(
-          onPressed: () {
-            // Adiciona uma ação para o botão
-            print("Botão Mesa clicado");
-          },
-          child: const Text(
-            "Mesa",
-            style: TextStyle(fontSize: 20),
-          ),
+
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.center, // Alinha os widgets horizontalmente
+           
+          children: [
+            // botões
+            ElevatedButton(
+              onPressed: () {
+
+                print("Botão Mesa foi clicado");
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 130, vertical:70),
+                shape: RoundedRectangleBorder(
+
+                  borderRadius: BorderRadius.circular(7.0)
+                ),
+
+                backgroundColor: const Color.fromARGB(255, 212, 133, 14)
+              ),
+
+              child: const Text( "Mesa", 
+
+                style: TextStyle(fontSize: 30,color: Colors.white),
+
+              ),
+            ),
+            SizedBox(height: 30), // Espaço entre os botões
+
+            ElevatedButton(
+
+              onPressed: () {
+
+                print("Botão Comanda foi clicado");
+              },
+              style: ElevatedButton.styleFrom(
+
+                padding: EdgeInsets.symmetric(horizontal: 100,vertical: 70),
+                shape: RoundedRectangleBorder(
+
+                  borderRadius:  BorderRadius.circular(7.0),
+
+                ),
+                backgroundColor: const Color.fromARGB(255, 245, 180, 0)
+
+              ),
+              child: const Text(
+
+                "Comanda",
+
+                style: TextStyle(fontSize: 30,color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
