7c1c623cddbdce8e5f1f78458f0d3889df820420
Felipe Antônio de O. Almeida <126277708+FelipeAlmeida06@users.noreply.github.com>
2024-10-02 18:06:22 -0300

    pages adicionais
    varias coisas

---
+++ cantinho_do_espeto_application/lib/pages/crud_pages/deletar_page.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Import necessário para FilteringTextInputFormatter
+
+class DeletarProduto extends StatefulWidget {
+  const DeletarProduto({super.key});
+
+  @override
+  _DeletarProdutoState createState() => _DeletarProdutoState();
+}
+
+class _DeletarProdutoState extends State<DeletarProduto> {
+  // Controladores para os campos de texto
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _precoController = TextEditingController();
+  String? _categoriaSelecionada; // Para o Dropdown de categorias
+
+  // Função de validação dos campos
+  void _validarCampos() {
+    String nomeProduto = _nomeController.text.trim();
+    String precoProduto = _precoController.text.trim();
+
+    if (nomeProduto.isEmpty ||
+        precoProduto.isEmpty ||
+        _categoriaSelecionada == null) {
+      // Exibe um alerta informando que os campos estão vazios
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Preencha os campos antes de continuar!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else if (!_precoValido(precoProduto)) {
+      // Se o preço contiver caracteres inválidos, exibe um alerta
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Preço inválido! Por favor, insira apenas números.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      // Se todos os campos estiverem preenchidos corretamente, exibe uma mensagem de sucesso
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Produto excluído com sucesso!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+      // Aqui você pode adicionar o código para continuar o processo de criação do produto
+    }
+  }
+
+  // Função para validar se o preço contém apenas números
+  bool _precoValido(String preco) {
+    // Verifica se o preço contém apenas números
+    final RegExp regExp = RegExp(r'^[0-9]+$');
+    return regExp.hasMatch(preco);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Excluir produto"),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(color: Colors.orange),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'Menu',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                  Builder(
+                    builder: (context) {
+                      return IconButton(
+                        icon: const Icon(Icons.close, color: Colors.white),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Início'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('#'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('##'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('###'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: 750,
+                height: 1250,
+                color: Colors.grey[300],
+                padding: const EdgeInsets.all(16.0),
+                margin: const EdgeInsets.only(bottom: 200),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Campo Nome do Produto
+                    TextField(
+                      controller: _nomeController,
+                      decoration: const InputDecoration(
+                        labelText: "Nome do produto",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    // Dropdown de Categoria
+                    Options(
+                      onCategoriaSelecionada: (String? categoria) {
+                        setState(() {
+                          _categoriaSelecionada = categoria;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 25),
+                    // Campo Preço do Produto
+                    TextField(
+                      controller: _precoController,
+                      decoration: const InputDecoration(
+                        labelText: "Preço do produto",
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20.0,
+                    horizontal: 40.0,
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  _validarCampos(); // Chama o método de validação
+                },
+                child: const Text(
+                  'Excluir',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20.0,
+                    horizontal: 40.0,
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, 'home');
+                },
+                child: const Text(
+                  "Voltar",
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 60),
+        ],
+      ),
+    );
+  }
+}
+
+class Options extends StatefulWidget {
+  final ValueChanged<String?> onCategoriaSelecionada;
+
+  const Options({required this.onCategoriaSelecionada, Key? key})
+      : super(key: key);
+
+  @override
+  _OptionsState createState() => _OptionsState();
+}
+
+class _OptionsState extends State<Options> {
+  String? _selecionarOpcoes;
+
+  final List<String> _opcoes = [
+    "Entrada",
+    "Prato Principal",
+    "Sobremesa",
+    "Bebida"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      hint: const Text("Selecione a categoria do produto"),
+      value: _selecionarOpcoes,
+      icon: const Icon(Icons.keyboard_arrow_down),
+      isExpanded: true,
+      items: _opcoes.map((String opcao) {
+        return DropdownMenuItem(
+          value: opcao,
+          child: Text(opcao),
+        );
+      }).toList(),
+      onChanged: (String? novoValor) {
+        setState(() {
+          _selecionarOpcoes = novoValor;
+        });
+        widget.onCategoriaSelecionada(novoValor);
+      },
+    );
+  }
+}
---
+++ cantinho_do_espeto_application/lib/pages/selecao_pages/cardapio_page.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TelaCardapio(),
+    );
+  }
+}
+
+class TelaCardapio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cardápio"),
+        backgroundColor: Colors.grey,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(color: Colors.orange),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Expanded(
+                    child: Text(
+                      "Menu",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    ),
+                  ),
+                  Builder(
+                    builder: (context) {
+                      return IconButton(
+                        icon: const Icon(Icons.close, color: Colors.white),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home), // home
+              title: const Text('Início - Cardápio'), // Início
+              onTap: () {
+                // navega para a tela de entrada
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.restaurant_menu), // home
+              title: const Text('Entrada'), // Início
+              onTap: () {
+                // navega para a tela de entrada
+                //Navigator.pushNamed(context, 'entrada');
+
+                // navega para a tela de entrada
+                _showLoadingThenNavigate(context, 'entrada');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.dinner_dining),
+              title: const Text('Prato Principal'),
+              onTap: () {
+                // navega para a tela de prato principal
+                //Navigator.pushNamed(context, 'prato principal');
+
+                // navega para a tela de prato principal
+                _showLoadingThenNavigate(context, 'prato principal');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.cake),
+              title: const Text('Sobremesa'),
+              onTap: () {
+                // navega para a tela de sobremesa
+                //Navigator.pushNamed(context, 'sobremesa');
+
+                // navega para a tela de sobremesa
+                _showLoadingThenNavigate(context, 'sobremesa');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.wine_bar),
+              title: const Text('Bebida'),
+              onTap: () {
+                // navega para a tela de bebida
+                //Navigator.pushNamed(context, 'bebida');
+
+                // navega para a tela de bebida
+                _showLoadingThenNavigate(context, 'bebida');
+              },
+            ),
+            const SizedBox(height: 20), // espaço entre os demais itens da lista
+            ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Sair'),
+              onTap: () {
+                // ação
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Botão Entrada
+            _buildCardButton(context, "Entrada"),
+
+            const SizedBox(height: 16.0),
+
+            // Botão Prato Principal
+            _buildCardButton(context, "Prato Principal"),
+
+            const SizedBox(height: 16.0),
+
+            // Botão Sobremesa
+            _buildCardButton(context, "Sobremesa"),
+
+            const SizedBox(height: 16.0),
+
+            // Botão Bebida
+            _buildCardButton(context, "Bebida"),
+
+            const SizedBox(height: 50.0),
+
+            // Botão Finalizar
+            _buildFinalizeButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Função para exibir loading antes de navegar para uma nova tela
+void _showLoadingThenNavigate(BuildContext context, String routeName) {
+  showDialog(
+    context: context,
+    barrierDismissible: false, // Evita fechar o loading clicando fora
+    builder: (BuildContext context) {
+      return const Center(
+        child: Column(
+          mainAxisSize: MainAxisSize
+              .min, // Para que o tamanho do dialog se ajuste ao conteúdo
+          children: [
+            CircularProgressIndicator(), // Indicador de loading
+            SizedBox(height: 16), // Espaçamento entre o indicador e o texto
+            Text(
+              "LOADING...",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+
+  // Simula um atraso de 2 segundos antes de navegar
+  Future.delayed(Duration(seconds: 2), () {
+    Navigator.pop(context); // Fecha o diálogo de loading
+    Navigator.pushNamed(context, routeName); // Navega para a próxima tela
+  });
+}
+
+// Função dos botões laranjas
+Widget _buildCardButton(BuildContext context, String text) {
+  return SizedBox(
+    width: double.infinity,
+    height: 80, // altura dos botões
+    child: ElevatedButton(
+      onPressed: () {
+        // ação
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.orange, // cor de fundo dos botões
+        foregroundColor: Colors.white, // cor do texto dos botões
+        textStyle: const TextStyle(fontSize: 18), // tamanho de fonte
+      ),
+      child: Text(text),
+    ),
+  );
+}
+
+// Função para botão 'Finalizar'
+Widget _buildFinalizeButton(BuildContext context) {
+  return SizedBox(
+    width: double.infinity,
+    height: 60,
+    child: ElevatedButton(
+        onPressed: () {
+          // ação
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          textStyle: const TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        child: const Text("Finalizar")),
+  );
+}
